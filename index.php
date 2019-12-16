e2127d1f7b60ad7441bc7b5649936f65122dea78
VorakGergo <vorakgeri@gmail.com>
2019-12-16 19:14:11 +0100

    bejelentkezési felület php-val

--- index.php
+++ index.php
@@ -1,28 +1,25 @@
 <?php
-  include("classes.php");
+  include("indexclasses.php");
   $session = new Session();
   $session->signinSessionStart(); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="hu">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Kosárlabda játék</title>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/awesome-bootstrap-checkbox.min.css" rel="stylesheet">
-    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">	 
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
   </head>
   <body>
-<section>
-  <span class="header-ribbon"/>
   <div class="container-fluid">
     <form method="POST">
 	  <div class="row">
         <div class="col-md-4 col-md-offset-4 col-centered">
             <div class="login-panel">
-				<center><h4 class="login-panel-title">Kosárlabda játék</h4></center>
+				<center><h1 class="login-panel-title">Kosárlabda játék</h1></center>
                 <div class="login-panel-section">
                     <div class="form-group">
                         <div class="input-group margin-bottom-sm">
@@ -40,33 +37,20 @@
                     </div>
                 <div class="login-panel-section">
 					<input type="hidden" name="action" value="cmd_signin">
-                    <input type="submit" class="btn btn-default" value="Bejelentkezés" id="loginbtn"> | <a href="#">Regisztráció</a>
+					<input type="submit" class="btn btn-default" value="Bejelentkezés" id="loginbtn"> | <a href="#">Regisztráció</a>
                 </div>
             </div>
         </div>
       </div>
 	  </form>
 	  </div>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
 <?php
         if(isset($_POST["action"]) && $_POST["action"] == "cmd_signin")
         {
           if(empty($_POST["input_felhasznalonev"]) || empty($_POST["input_jelszo"]))
-          { ?>
-            <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content error-modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="errorModalLabel">Error</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div class="modal-body">You have to fill all the blank!</div>
-                </div>
-              </div>
-            </div> <?php
+          {?>
+			  <script>alert("Nem adta meg a felhasználónevet, vagy a jelszót!")</script>
+	  		<?php
           }
           else
           {     
@@ -76,6 +60,5 @@
             $data->disconnect();
           }
         }?>
-</section>
   </body>
 </html>
